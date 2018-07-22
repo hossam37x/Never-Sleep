97d62c955a8e474b80bcd83efbd51e0dc4224356
hossam <hossammohsen37@gmail.com>
2018-07-22 02:35:24 +0200

    First

--- app/src/main/java/com/example/hossam/neversleep/Activities/UserSelectionActivity.java
+++ app/src/main/java/com/example/hossam/neversleep/Activities/UserSelectionActivity.java
@@ -1,16 +1,29 @@
 package com.example.hossam.neversleep.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.example.hossam.neversleep.Database.ApplicationDatabase;
+import com.example.hossam.neversleep.Database.Model.User;
 import com.example.hossam.neversleep.R;
+import com.example.hossam.neversleep.UserAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 
 public class UserSelectionActivity extends AppCompatActivity {
 
+    @BindView(R.id.user_recycler_view)
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +35,22 @@ public class UserSelectionActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
-    }
+        ApplicationDatabase applicationDatabase = new ApplicationDatabase(this);
+        ArrayList<User> users = new ArrayList<>(applicationDatabase.getAllUsers());
+        UserAdapter userAdapter = new UserAdapter(users, new UserAdapter.RecyclerItemClickListener() {
+            @Override
+            public void onItemClick(User user) {
+                Intent intent = new Intent(UserSelectionActivity.this,MainActivity.class);
+                intent.putExtra(MainActivity.CURRENT_USER, user);
+                startActivity(intent);
+            }
+        });
 
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(userAdapter);
+    }
 }
--- app/src/main/java/com/example/hossam/neversleep/UserAdapter.java
+++ app/src/main/java/com/example/hossam/neversleep/UserAdapter.java
@@ -5,20 +5,28 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.hossam.neversleep.Database.Model.User;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class UserAdapter extends RecyclerView.Adapter {
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
 
-    ArrayList<Object> Users;
+    ArrayList<User> Users;
+    RecyclerItemClickListener mlistener;
 
-    public UserAdapter(ArrayList<Object> users) {
+    public UserAdapter(ArrayList<User> users,RecyclerItemClickListener listener)
+    {
         Users = users;
+        this.mlistener = listener;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.user_reccyclerview_item,parent,false );
 
@@ -27,19 +35,37 @@ public class UserAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position)
+    {
+        holder.onBind(Users.get(position), mlistener);
     }
 
     @Override
     public int getItemCount() {
         return Users.size();
     }
-    private class UserViewHolder extends RecyclerView.ViewHolder
+    public class UserViewHolder extends RecyclerView.ViewHolder
     {
+        User user;
+        private TextView textView;
         public UserViewHolder(View itemView) {
             super(itemView);
-
+            textView = (TextView)itemView.findViewById(R.id.user_recycler_view_text);
         }
+        void onBind(User user, final RecyclerItemClickListener listener)
+        {
+            this.user = user;
+            textView.setText(user.getName());
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(UserViewHolder.this.user);
+                }
+            });
+        }
+    }
+    public interface RecyclerItemClickListener
+    {
+        void onItemClick(User user);
     }
 }
